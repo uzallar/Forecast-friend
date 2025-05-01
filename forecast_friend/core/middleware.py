3993196c960dfd9cbdd8dd2a26c66ac0f28dd8b5
margin2031 <serbakovd187@gmail.com>
2025-05-01 18:25:55 +0300

    исправил отзывы

--- forecast_friend/core/middleware.py
+++ forecast_friend/core/middleware.py
@@ -13,7 +13,7 @@ class LoginRequiredMiddleware:
     def __call__(self, request):
         login_url = reverse('login')
         # страницы, на которые можно без авторизации
-        allowed_urls = [login_url, reverse('register')]  
+        allowed_urls = [login_url, reverse('register'), reverse('review_page')]  
 
         if not request.user.is_authenticated and request.path not in allowed_urls:
             return redirect(login_url)
